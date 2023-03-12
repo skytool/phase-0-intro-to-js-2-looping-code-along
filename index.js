90a40ab602db84680611beb05affab6cd6f75860
skytool <albert.do@aol.com>
2023-03-11 23:15:12 -0600

    keep goinggit add .

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const array = [];
+
+function writeCards(names, event){
+    for(let i=0;i<names.length;i++){
+        let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        array.push(message);    
+    }
+    return array;
+}
+
+function countDown(){
+    let num = 10;
+    while(num >= 0){
+        
+        console.log(num);
+        num--;
+    }
+}
